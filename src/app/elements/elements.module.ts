60bef651b89789984adfb6f7b3c415e6047c193f
= <=>
2020-07-05 21:58:33 +0600

    widget module added and ng-content added

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
-  imports: [CommonModule, ElementsRoutingModule],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [ElementsHomeComponent],
 })
 export class ElementsModule {}
