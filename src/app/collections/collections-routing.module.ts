4066402cd8ca1d546ae227fe3905ed337dccb818
= <=>
2020-07-06 12:04:48 +0600

    tab menu added

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from '../collections/biography/biography.component';
+import { CompaniesComponent } from '../collections/companies/companies.component';
+import { PartnersComponent } from '../collections/partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CollectionsHomeComponent,
+    children: [
+      {
+        path: '',
+        component: BiographyComponent,
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent,
+      },
+    ],
   },
 ];
 
