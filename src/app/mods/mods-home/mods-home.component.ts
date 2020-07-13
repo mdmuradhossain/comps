2b99a59e054896b08143d81e6c268e53f1e1056f
= <=>
2020-07-13 14:29:39 +0600

    finish

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
-
+  items = [
+    {
+      title: 'Why us the sky blue',
+      content: 'The Sky is blue becuase this the way how its created.',
+    },
+    {
+      title: 'What does an orange taste like?',
+      content: 'A orange taste like sweet and bitter',
+    },
+    {
+      title: 'What color is that cat',
+      content: 'The cat is an white and black color',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
